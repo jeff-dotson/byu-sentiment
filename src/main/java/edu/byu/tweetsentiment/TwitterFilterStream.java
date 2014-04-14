f57f5e2eba5dba0a3995eb9f0f6329a0c7f5cdc9
Rob <robmonc@gmail.com>
2014-04-14 15:49:39 -0600

    Adding the "keep going" message to make the while look keep going.

--- src/main/java/edu/byu/tweetsentiment/TwitterFilterStream.java
+++ src/main/java/edu/byu/tweetsentiment/TwitterFilterStream.java
@@ -65,9 +65,10 @@ public class TwitterFilterStream {
         // Establish a connection
         client.connect();
 
+        boolean keepgoing = true;
         // Reading a message from twitter, calculating the sentiment, then inserting it into the database
         //for (int msgRead = 0; msgRead < 100000; msgRead++) {
-        while(true){
+        while(keepgoing){
             String msg = queue.take();
             //System.out.println(msgRead + ") " + msg);
             insertTweet(msg);   //Analyzes sentiment, sends tweet to the db
